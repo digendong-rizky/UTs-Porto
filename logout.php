9a03d7a2696125cabac37db5f988268c6833b372
STRIKER313 <daniel2006wijaya@gmail.com>
2025-11-13 16:20:31 +0700

    hapus

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: index.php");
-exit();
-Leader commit & push:
-git add .
-git commit -m "Inisialisasi project login"
-git push origin main
